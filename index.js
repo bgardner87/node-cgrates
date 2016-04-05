27cde514d2af3cbaa547359fbb2096544f35ee99
Brad Gardner <bgardner87@gmail.com>
2016-04-05 18:35:41 -0400

    Added getAccount

--- index.js
+++ index.js
@@ -48,26 +48,57 @@ var CGRates = function(url) {
 		return promise;
 	};
 
-	
+	this.getAccount = function(options, request_id) {
+		if (!options.Tenant) {
+			throw new Error("Tenant is required");
+		}
+
+		if (!options.Account) {
+			throw new Error("Account is required");
+		}
+
+		var data = {
+			method: "ApierV2.GetAccount",
+			params: [options]
+		};
+
+		if (request_id) {
+			data.id = request_id;
+		}
+
+		var promise = new Promise(function(resolve, reject) {
+			self.needle.post(self.url, data, {json:true}, function(err, response) {
+				if (err) {
+					return reject(err);
+				}
+
+				if (response.statusCode != 200) {
+					return reject(new Error(response.body));
+				}
+
+				if (response.body.error) {
+					return reject(new Error(response.body.error));
+				}
+
+				resolve(response.body.result);
+			});
+		});
+		
+		return promise;
+	};
 
 	return this;
 }
 
 module.exports = CGRates;
 
-// var cgRates = new CGRates("http://172.20.4.13:2080/jsonrpc");
+var cgRates = new CGRates("http://172.20.4.13:2080/jsonrpc");
 
-// var data = {Tenant: "reper.io"};
+var data = {Tenant: "reper.io", Account: "f1ed1023-b6a0-4140-ad4c-5475006f86d3"};
 
-// cgRates.getAccounts(data).then(function(result) {
-// 	console.log(result);
-// }, function(err) {
-// 	console.log("ERROR");
-// 	console.log(err);
-// });
-
-
-// needle.get('http://www.google.com', function(error, response) {
-//   if (!error && response.statusCode == 200)
-//     console.log(response.body);
-// });
+cgRates.getAccount(data).then(function(result) {
+	console.log(result);
+}, function(err) {
+	console.log("ERROR");
+	console.log(err);
+});
--- tests.js
+++ tests.js
@@ -16,7 +16,7 @@ describe("CGRates", function() {
 	describe("Accounts", function() {
 		describe("GetAccounts", function() {
 			it("should require tenant", function() {
-				assert.throws(() => {cgRates.getAccounts()}, Error, "Didn't throw exception when tenant is missing.");
+				assert.throws(() => {cgRates.getAccounts({})}, function(err){return err.message === "Tenant is required";}, "Didn't throw exception when tenant is missing.");
 			});
 
 			it("should reject if the http request fails", function(done) {
@@ -67,6 +67,72 @@ describe("CGRates", function() {
 				};
 
 				var promise = cgRates.getAccounts({Tenant:'test'});
+
+				promise.then(function(result) {
+					done();
+				}, function(err) {
+					done(new Error("Promise rejected on valid request and response."));
+				})
+			});
+		});
+
+		describe("GetAccount", function() {
+			it("should require tenant", function() {
+				assert.throws(() => {cgRates.getAccount({})}, function(err){return err.message === "Tenant is required";}, "Didn't throw exception when tenant is missing.");
+			});
+
+			it("should require account", function() {
+				assert.throws(() => {cgRates.getAccount({Tenant:'123'})}, function(err){return err.message === "Account is required";}, "Didn't throw exception when tenant is missing.");
+			});
+
+			it("should reject if the http request fails", function(done) {
+				cgRates.needle.post = function(url, data, options, callback) {
+					callback(new Error('test'));
+				};
+
+				var promise = cgRates.getAccount({Tenant:'test', Account:'123'});
+
+				promise.then(function(result) {
+					done(new Error("Promise resolved on failed http request."));
+				}, function(err) {
+					done();
+				})
+			});
+
+			it("should reject if the http status code is not 200", function(done) {
+				cgRates.needle.post = function(url, data, options, callback) {
+					callback(null, {statusCode: 500});
+				};
+
+				var promise = cgRates.getAccount({Tenant:'test', Account:'123'});
+
+				promise.then(function(result) {
+					done(new Error("Promise resolved on failed http request."));
+				}, function(err) {
+					done();
+				})
+			});
+
+			it("should reject if the http response body has an error from CG Rates", function(done) {
+				cgRates.needle.post = function(url, data, options, callback) {
+					callback(null, {statusCode: 200, body:{error:"error"}});
+				};
+
+				var promise = cgRates.getAccount({Tenant:'test', Account:'123'});
+
+				promise.then(function(result) {
+					done(new Error("Promise resolved on failed http request."));
+				}, function(err) {
+					done();
+				})
+			});
+
+			it("should resolve with the request body result", function(done) {
+				cgRates.needle.post = function(url, data, options, callback) {
+					callback(null, {statusCode: 200, body:{result:{id:1}}});
+				};
+
+				var promise = cgRates.getAccount({Tenant:'test', Account:'123'});
 
 				promise.then(function(result) {
 					done();
