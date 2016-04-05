98da50d6070828d14d39c16ca988492eb7452067
Brad Gardner <bgardner87@gmail.com>
2016-04-05 18:45:45 -0400

    finished account CRUD functions

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ var CGRates = function(url) {
 
 
 
-	//{"id": 3, "method": "ApierV2.GetAccounts", "params": [{"Tenant": "domain.com"}]}
+	// {
+	// 	"id": 1,
+	// 	"method": "ApierV2.GetAccounts",
+	// 	"params": [{
+	// 		"Tenant": "domain.com",
+	// 		"AccountIds": "", //optional
+	// 		"Offset": 0, //optional
+	// 		"Limit": 0 //optional
+	// 	}]
+	// }
 	this.getAccounts = function(options, request_id) {
 		if (!options.Tenant) {
 			throw new Error("Tenant is required");
@@ -48,6 +57,14 @@ var CGRates = function(url) {
 		return promise;
 	};
 
+	// {
+	// 	"id": 1,
+	// 	"method": "ApierV2.GetAccount",
+	// 	"params": [{
+	// 		"Tenant": "domain.com",
+	// 		"Account": "5c1b474f-3a10-48d8-89a4-395a94a95a3c"
+	// 	}]
+	// }
 	this.getAccount = function(options, request_id) {
 		if (!options.Tenant) {
 			throw new Error("Tenant is required");
@@ -59,6 +76,105 @@ var CGRates = function(url) {
 
 		var data = {
 			method: "ApierV2.GetAccount",
+			params: [options]
+		};
+
+		if (request_id) {
+			data.id = request_id;
+		}
+
+		var promise = new Promise(function(resolve, reject) {
+			self.needle.post(self.url, data, {json:true}, function(err, response) {
+				if (err) {
+					return reject(err);
+				}
+
+				if (response.statusCode != 200) {
+					return reject(new Error(response.body));
+				}
+
+				if (response.body.error) {
+					return reject(new Error(response.body.error));
+				}
+
+				resolve(response.body.result);
+			});
+		});
+		
+		return promise;
+	};
+
+	// {
+	// 	"id": 1,
+	// 	"method": "ApierV2.SetAccount",
+	// 	"params": [{
+	// 		"Tenant": "domain.com",
+	// 		"Account": "5c1b474f-3a10-48d8-89a4-395a94a95a3c",
+	// 		"ActionPlanId": "", //optional
+	// 		"ActionTriggersId": "", //optional
+	// 		"AllowNegative": true, //optional
+	// 		"Disabled": false, //optional
+	// 		"ReloadScheduler": true //optional
+	// 	}]
+	// }
+	this.setAccount = function(options, request_id) {
+		if (!options.Tenant) {
+			throw new Error("Tenant is required");
+		}
+
+		if (!options.Account) {
+			throw new Error("Account is required");
+		}
+
+		var data = {
+			method: "ApierV2.SetAccount",
+			params: [options]
+		};
+
+		if (request_id) {
+			data.id = request_id;
+		}
+
+		var promise = new Promise(function(resolve, reject) {
+			self.needle.post(self.url, data, {json:true}, function(err, response) {
+				if (err) {
+					return reject(err);
+				}
+
+				if (response.statusCode != 200) {
+					return reject(new Error(response.body));
+				}
+
+				if (response.body.error) {
+					return reject(new Error(response.body.error));
+				}
+
+				resolve(response.body.result);
+			});
+		});
+		
+		return promise;
+	};
+
+	// {
+	// 	"id": 1,
+	// 	"method": "ApierV2.RemoveAccount",
+	// 	"params": [{
+	// 		"Tenant": "domain.com",
+	// 		"Account": "5c1b474f-3a10-48d8-89a4-395a94a95a3c"
+	// 	}]
+	// }
+	this.removeAccount = function(options, request_id) {
+		if (!options.Tenant) {
+			throw new Error("Tenant is required");
+		}
+
+		if (!options.Account) {
+			throw new Error("Account is required");
+		}
+
+		var data = {
+			method: "ApierV2.RemoveAccount",
 			params: [options]
 		};
 
